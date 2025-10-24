a876ce3e4fe632422c963358c67ebc01eee721d9
HakimSky <l200220090@student.ums.ac.id>
2025-10-25 04:24:34 +0700

    Menaruh template
    template

--- project/project/urls.py
+++ project/project/urls.py
@@ -1,22 +1,26 @@
-"""
-URL configuration for project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # Halaman utama (Home)
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+     # Halaman Shop List
+    path('shop/', TemplateView.as_view(template_name='shop_list.html'), name='shop'),
+    # Halaman Shop Detail
+    path('shop/detail/', TemplateView.as_view(template_name='shop_detail.html'), name='shop_detail'),
+    path('', include('transactions.urls')),
+    path('contact/', TemplateView.as_view(template_name='contact.html'), name='contact'),
+
+
 ]
+
+# Supaya file static (CSS, JS, IMG) bisa diakses saat DEBUG=True
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- project/transactions/views.py
+++ project/transactions/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def cart_view(request):
+    return render(request, 'transactions/cart.html')
+
+def checkout_view(request):
+    return render(request, 'transactions/checkout.html')
